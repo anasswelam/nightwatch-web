2084eebf20db7a41346230189fc78fc26d82f1b1
alexandlazaris <alex.lazaris617@gmail.com>
2017-09-20 17:05:42 +1000

    Modify script to return a test failure to give variation to report. Data is too perfect

--- tests/npm/test_links.js
+++ tests/npm/test_links.js
@@ -5,36 +5,36 @@ module.exports = {
     'Load npm homepage' : function (browser) {
         var home = browser.page.homepage()
         browser.init()//this auto grabs launch_url value from nightwatch.json
-        browser.assert.title("npm");
-    },
+        browser.verify.title("google");
+    },//something failing here ... need to continue test even if failed
     'Load enterprise page' : function (browser) {
         var home = browser.page.homepage()
         browser.init()
         home.goToEnterprise()
-        browser.assert.title('npm Enterprise');
+        browser.verify.title('npm Enterprise');
     },
     'Load features page' : function (browser) {
         var home = browser.page.homepage()
         browser.init()
         home.goToFeatures()
-        browser.assert.title('npm');
+        browser.verify.title('npm');
     },
     'Load pricing page' : function (browser) {
         var home = browser.page.homepage()
         browser.init()
         home.goToPricing()
-        browser.assert.title('npm');
+        browser.verify.title('npm');
     },
     'Load documentation page' : function (browser) {
         var home = browser.page.homepage()
         browser.init()
         home.goToDocumentation()
-        browser.assert.title('npm Documentation');
+        browser.verify.title('npm Documentation');
     },
     'Load support page' : function (browser) {
         var home = browser.page.homepage()
         browser.init()
         home.goToSupport()
-        browser.assert.title('npm');
+        browser.verify.title('npm');
     }
 };
